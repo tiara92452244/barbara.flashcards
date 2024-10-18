bc2ead4ba199cc9edf58d96d708f1a11f43ba9d8
tiara92452244 <144949918+tiara92452244@users.noreply.github.com>
2024-10-17 22:22:52 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'qual é a capital do japão',
+    'tóquio '
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da alemanha',
+    'berlim'
+)
+
+criaCartao(
+    'História',
+    'quando ocorreu a queda do império romano?',
+    'em 476 d.c.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'o que significa ( work)',
+    'trabalho'
+)
